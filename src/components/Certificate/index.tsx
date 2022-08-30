f320b867cb7314f9c3f6931ea2818a4ad0496bd5
rpxic <rpc2works@gmail.com>
2022-08-30 09:07:44 +0200

    fix border radius

--- src/components/Certificate/index.tsx
+++ src/components/Certificate/index.tsx
@@ -1,8 +1,14 @@
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import * as certificateStyles from './certificate.module.css'
 
 const Certificate = ({ certificate }) => {
   const image = getImage(certificate)
-  return <GatsbyImage alt='certificate' image={image} style={{ borderRadius: '1rem' }} />
+
+  return (
+    <div className={certificateStyles.container}>
+      <GatsbyImage alt='certificate' image={image} style={{ borderRadius: '1rem' }} />
+    </div>
+  )
 }
 
 export default Certificate
