7b377d20d4ae04346a30c3c4bd931b62a36bcd63
rpxic <rpc2works@gmail.com>
2022-12-22 22:43:40 +0100

    about changes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -8,15 +8,21 @@ const Home = () => (
         <h1>Ruben Ponce</h1>
       </div>
       <h2 className={homeStyles.text}>
-        I'm a Full Stack Developer from Barcelona. I was graduated in{' '}
-        <a href='https://www.skylabcoders.com/es' target='_blank' rel='noreferrer'>
-          Skylab Coders Academy
-        </a>
-        , and I'm currently working at{' '}
-        <a href='https://www.spotlio.com/' target='_blank' rel='noreferrer'>
-          Spotlio
-        </a>
-        . You can take a look at my projects and technologies that I have used. Anything, feel free to contact me.
+        <span>I'm a Full Stack Developer based in Barcelona, currently searching for a new position.</span>
+        <br />
+        <span>
+          I was graduated from{' '}
+          <a href='https://www.skylabcoders.com/es' target='_blank' rel='noreferrer'>
+            Skylab Coders Academy
+          </a>
+          , and I worked at{' '}
+          <a href='https://www.spotlio.com/' target='_blank' rel='noreferrer'>
+            Spotlio
+          </a>{' '}
+          as a Front End and Mobile Apps Developer.
+        </span>
+        <br /> <span>You can take a look at my projects and technologies that I have used.</span>
+        <br /> <span>Anything, feel free to contact me.</span>
       </h2>
     </section>
     <Stacks />
